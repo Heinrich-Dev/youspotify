cf1dc4bb3b4f52853468fcf0b74ea8f37c49a066
Heinrich-Dev <boekhoffhenry@gmail.com>
2024-03-06 19:11:31 -0800

    icons now darken when hovered over:

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ button.addEventListener('click', () => {
     alert('Clicked!')
 })
 document.body.appendChild(button)
+
+function darken_img(x){
+    x.style.filter = "brightness(50%)";
+}
+function brighten_img(x){
+    x.style.filter = "brightness(100%)";
+}
