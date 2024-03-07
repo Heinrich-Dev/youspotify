f52a754b87531948742d09d1b3a0f5c72640201e
Heinrich-Dev <boekhoffhenry@gmail.com>
2024-03-06 18:39:03 -0800

    beginnings of frontend

---
+++ app.js
@@ -0,0 +1,6 @@
+const button = document.createElement('button')
+button.innerText = 'Get TinyDesk Playlist'
+button.addEventListener('click', () => {
+    alert('Clicked!')
+})
+document.body.appendChild(button)
